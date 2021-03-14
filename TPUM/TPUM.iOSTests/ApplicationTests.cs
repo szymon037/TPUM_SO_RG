3a34c6ce1a89f97b199bdc9a74d2e13d74dd722b
Muroidea <rafii.gora@gmail.com>
2021-03-14 16:12:04 +0100

    Added example project

---
+++ TPUM/TPUM.iOSTests/ApplicationTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TPUM.iOS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPUM.iOS.Tests
+{
+    [TestClass()]
+    public class ApplicationTests
+    {
+        [TestMethod()]
+        public void GreaterThanZeroTest()
+        {
+            Assert.IsTrue(Application.GreaterThanZero(1));
+            Assert.IsFalse(Application.GreaterThanZero(0));
+            Assert.IsFalse(Application.GreaterThanZero(-1));
+        }
+    }
+}
---
+++ TPUM/TPUM/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using TPUM.Models;
+using TPUM.ViewModels;
+
+namespace TPUM.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
