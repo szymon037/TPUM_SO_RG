52f02b1a14c8114a78504b890e87908ed93acd2b
Szymon Omieciński <szymon.lost@gmail.com>
2021-04-18 16:25:34 +0200

    Added base UnitTest for Data layer

---
+++ DataTests/Collections/BookCollectionTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Data.Collections;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Collections.Tests
+{
+    [TestClass()]
+    public class BookCollectionTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetTest2()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
